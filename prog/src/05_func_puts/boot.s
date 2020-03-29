8f0dec1c69a9e3cce7adaa13779d48ab3e7ccdac
cotton392 <e1739@s.akashi.ac.jp>
2020-03-29 19:03:12 +0900

    文字列を出力する関数を実装

---
+++ prog/src/05_func_puts/boot.s
@@ -0,0 +1,37 @@
+        BOOT_LOAD       equ     0x7C00          ;ブートプログラムのロード位置
+
+        ORG     BOOT_LOAD
+
+%include    "../include/macro.s"
+
+entry:                                          ;エントリポイント
+        jmp     ipl                             ;IPLへジャンプ
+
+        times   90 - ($ - $$) db 0x90           ;BPB(BIOS Parameter Block)
+        
+ipl:                                            ;IPL(Initial Program Loader)
+        cli                                     ;割り込み禁止
+
+        mov     ax, 0x0000
+        mov     ds, ax
+        mov     es, ax
+        mov     ss, ax
+        mov     sp, BOOT_LOAD
+
+        sti                                     ;割り込み許可
+        mov     [BOOT.DRIVE], dl                ;ブートドライブを保存
+
+        cdecl   puts, .s0                       ;文字列の表示
+
+        jmp     $
+
+.s0     db "booting...", 0x0A, 0x0D, 0          ;データ定義
+
+ALIGN 2, db 0
+BOOT:
+.DRIVE:         dw 0
+
+%include    "../modules/real/puts.s"
+
+        times   510 - ($ - $$) db 0x00          ;ブートフラグ(先頭512バイトの終了)
+        db      0x55, 0xAA
---
+++ prog/src/modules/real/puts.s
@@ -0,0 +1,32 @@
+puts:
+        push    bp                              ;スタックフレームの構築
+        mov     bp, sp
+
+        push    ax                              ;レジスタの保存
+        push    bx
+        push    si
+
+        mov     si, [bp + 4]                    ;SIに文字列のアドレスを代入
+
+        mov     ah, 0x0E                        ;テレタイプ式で1文字出力
+        mov     bx, 0x0000                      ;ページ番号と文字色を0に設定
+        cld                                     ;DFに0を代入しアドレスを加算
+
+.10L:
+        lodsb
+
+        cmp     al, 0                           ;ALが0である場合break、そうでない場合は文字を出力して.10Lの先頭に戻る
+        je      .10E
+
+        int     0x10
+        jmp     .10L
+
+.10E:
+        pop     si                              ;レジスタの復帰
+        pop     bx
+        pop     ax
+
+        mov     sp, bp                          ;スタックフレームの破棄
+        pop     bp
+
+        ret
